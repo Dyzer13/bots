1ed554e07e823622f0b33c350aee880bdc7cb18d
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-10-25 01:44:21 +0100

    Update bot4.js

--- bot4.js
+++ bot4.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
 client.on('guildMemberAdd' ,member => {
   if(member.guild.id !== '342590736090529794') return;
   setTimeout(function(){
-  member.guild.channels.find(r => r.id === '497176831431278612').send('**Welcome to Venom Network :rose:  :new_moon_with_face: **');
+  member.guild.channels.find(r => r.id === '497176831431278612').send('**#Welcome to __Venom Network__ :venom: :  :520428: **');
 },8000);
 });
 
